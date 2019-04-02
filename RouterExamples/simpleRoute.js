f9ada7deae77b752087b75e65e2bb31bccfd38df
aamirpinger <aamirpinger@yahoo.com>
2019-04-02 20:26:22 +0500

    React Router url-params, switch, simple route code fixed

--- RouterExamples/simpleRoute.js
+++ RouterExamples/simpleRoute.js
@@ -1,3 +1,10 @@
+// ************************************************************************************************
+//  This example illustrate how add simple routes in the react app
+//
+//  App.js file
+//
+// *************************************************************************************************
+
 import React, { Component, Fragment } from 'react'
 import { Link, Route } from 'react-router-dom'
 
